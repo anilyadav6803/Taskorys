6865dbe1aa0a00db685954b153f57ba2d3166658
anil yadav <anilyadav6803@gmail.com>
2025-04-06 00:11:57 +0530

    response

--- src/helper/responseMessage.js
+++ src/helper/responseMessage.js
@@ -1,9 +1,13 @@
-
-
-
 import { NextResponse } from "next/server";
 
-export const responseMessage = {
-    success: (message) => NextResponse.json({ message }, { status: 200 }),
-    error: (message) => NextResponse.json({ message }, { status: 500 }),
-  };
+export const getresponseMessage = (statuscode, message, successStatus) => {
+    return NextResponse.json(
+        {
+            message: message,
+            success: successStatus,
+        },
+        {
+            status: statuscode,
+        }
+    );
+};
